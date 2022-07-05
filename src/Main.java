19af631f94f574d56e19b0b6e0af1d40034ea041
jA71 <isjavier77@gmail.com>
2022-07-05 17:53:50 -0500

    Se sube el proyecto con la primera del 2do ejercicio del BootCamp

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+        /*
+        Usando un if, crear una condición que compare si la variable numeroIf es positivo, negativo, o 0.
+        Pista: Los números inferiores a 0 son negativos y los superiores, positivos
+        */
+        int numeroIf = 10;
+        if (numeroIf == 0) {
+            System.out.println("El número ingresado es 0");
+        }
+        else if (numeroIf >= 0) {
+            System.out.println("El número ingresado es positivo");{
+        }
+
+        } else {
+            System.out.println("El número ingresado es negativo");
+        }
+        /*
+        Usando un if, crear una condición que compare si la variable numeroIf es positivo, negativo, o 0.
+        Pista: Los números inferiores a 0 son negativos y los superiores, positivos
+        */
+
+    }
+}

1b87f77850b10dd5e390f0bcbaaeae26371563a7
jA71 <isjavier77@gmail.com>
2022-07-05 22:06:41 -0500

    3er-4to-5to apartados del 2do ejercicio del BootCamp

--- src/Main.java
+++ src/Main.java
@@ -18,8 +18,8 @@ public class Main {
         /*
         Crea un bucle While, este bucle tendrá que tener como condición que la variable numeroWhile sea inferior a 3,
         el bloque de código que tendrá el bucle deberá:Incrementar el valor de la variable en uno cada vez que se ejecute.
-        Mostrarlo por pantalla cada vez que se ejecute.
-        */
+        Mostrarlo por pantalla cada vez que se ejecute. */
+
 
         int numeroWhile = 2;
         while (numeroWhile >=2){
@@ -27,12 +27,51 @@ public class Main {
             numeroWhile = numeroWhile+1;
         }
 
+
         /*
-        Crea un bucle While, este bucle tendrá que tener como condición que la variable numeroWhile sea inferior a 3,
-        el bloque de código que tendrá el bucle deberá:Incrementar el valor de la variable en uno cada vez que se ejecute.
-        Mostrarlo por pantalla cada vez que se ejecute.
+        Para el bucle Do While, deberás crear la misma estructura que en el While, pero solo se debe ejecutar una vez.
         */
 
+        int numeroWhile = 2;
+        do{
+            System.out.println(numeroWhile);
+            numeroWhile = numeroWhile + 1;
+        } while (numeroWhile <2);
+
+        /*
+        Para el bucle For, crea una variable numeroFor, esta variable tendrá como valor 0 y su condición será que la
+        variable sea igual o menor que 3, se irá incrementando en 1 su valor cada vez que se ejecute y deberá
+        mostrarse por pantalla.
+        */
+
+        for (int numeroFor = 0; numeroFor<=3; numeroFor=numeroFor+1){
+            System.out.println(numeroFor);
+        }
+
+        /*
+        Por último, para el Switch, deberás crear la variable estacion, y distintos case para las cuatro
+        estaciones del año. Dependiendo del valor de la variable estacion se deberá mandar un mensaje por
+        consola informando de la estación en la que está. También habrá que poner un default para cuando
+        el valor de la variable no sea una estación.
+        */
+
+        var estacion = "Verano";
+        switch (estacion){
+            case "Verano":
+                System.out.println("Estamos en Verano");
+                break;
+            case "Invierno":
+                System.out.println("Estamos en Invierno");
+                break;
+            case "Otoño":
+                System.out.println("Estamos en Otoño");
+                break;
+            case "Primavera":
+                System.out.println("Estamos en Primavera");
+                break;
+            default:
+                System.out.println("ERROR ! NO ES NINGUNA ESTACIÓN");
+        }
 
 
     }
